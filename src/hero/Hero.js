7e8ab949994357df401f105a121aa30696828822
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2022-01-06 10:30:56 -0600

    added image to hero section
    adjusted flex for the hero section as well.

--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { HeroAbout } from './hero-about/HeroAbout';
-import './hero.scss';
+import {HeroAbout} from './hero-about/HeroAbout';
 import Social from './social-contact/Social';
+import HeroImage from './hero-image/HeroImage';
+import './hero.scss';
 
 export const Hero = () => {
     return (
         <div className='hero__content'>
-           <Social />
            <HeroAbout />
+           <HeroImage />
+           <Social />
         </div>
     )
 }
---
+++ src/hero/hero-image/HeroImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './heroimage.scss'
+
+export default function HeroImage() {
+    return (
+        <div className='hero__image'>
+            <img src='./hero_with_bg.png' width="500px" height="100%"/>
+        </div>
+    )
+}
